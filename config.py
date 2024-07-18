df3c588c0c4eeb6941b1b83e0ea2aa9d63732249
Candy-20 <kitty5789ooo@gmail.com>
2024-07-18 20:32:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7161771150:AAFWyKWD32aj70UDaEp8NKR0Utfaj5rler8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Siddikulla007")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7037294122")
 
 
 
