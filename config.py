e39d320780225e64529eb2515c84e8ff9d8c0bff
Candy-20 <kitty5789ooo@gmail.com>
2024-07-18 20:14:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7069705098:AAGrp8L4JXUv6yR7mDE_VBUGxkSEgku-HN8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Siddikulla007")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7037294122")
 
 
 
